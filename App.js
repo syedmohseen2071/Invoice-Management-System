ce9a6bebd19187aa75352dfdb44653b1fbe2e7ef
syedmohseen2071 <syedmohseen2071@gmail.com>
2026-01-04 23:11:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import InvoiceForm from "./pages/InvoiceForm";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/invoice" element={<InvoiceForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
